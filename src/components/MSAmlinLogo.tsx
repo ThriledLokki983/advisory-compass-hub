1b796d920e680b15de719875365fb88e437a4145
Gideon Nimoh <gideon.nimoh@pwc.com>
2025-04-12 10:32:48 +0200

    Added the correct logo

--- src/components/MSAmlinLogo.tsx
+++ src/components/MSAmlinLogo.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
+import Logo from '../img/amlin-logo.png';
 
 const MSAmlinLogo: React.FC = () => {
   return (
     <div className="flex items-center">
-      <div className="h-8 w-8 bg-amlin-red rounded-full flex items-center justify-center text-white font-bold mr-2">
-        M
-      </div>
-      <div className="font-bold text-amlin-blue">
-        <span>MS</span><span>Amlin</span>
-      </div>
+      <figure>
+        <img src={Logo} alt="AMlin Logo" />
+      </figure>
     </div>
   );
 };
